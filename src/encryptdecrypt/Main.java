8e5cf785d8e2c8a02de05dc587cf55219d898d9a
avblm26 <14940699+MikeAVB@users.noreply.github.com>
2020-03-13 11:30:49 +0300

    Added: 1) shift cipher algorithm 2) refactor by using strategy pattern ;-)

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -2,90 +2,283 @@ package encryptdecrypt;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
-        String mode = "enc";
-        int key = 0;
-        String data = "";
-        String inputFileName = "";
-        String outputFileName = "";
-
-        boolean dataDefined = false;
-        boolean fileInput = false;
-        boolean fileOutput = false;
-
         try {
-            for (int i = 0; i < args.length - 1; i += 2) {
-                String arg = args[i];
-                String param = args[i + 1];
-                if (arg.equals("-mode")) {
-                    mode = param;
-                } else if (arg.equals("-key")) {
-                    key = Integer.parseInt(param);
-                } else if (arg.equals("-data")) {
-                    data = param;
-                    dataDefined = true;
-                } else if (arg.equals("-in")) {
-                    inputFileName = param;
-                    fileInput = true;
-                } else if (arg.equals("-out")) {
-                    outputFileName = param;
-                    fileOutput = true;
-                } else {
-                    throw new IllegalArgumentException();
-                }
+            Options options = Options.fromArgs(args);
+            Context context = new Context(options.getMode(), options.getAlgorithm());
+            String data;
+            if (options.isDataDefined()) {
+                data = options.getData();
+            } else {
+                data = Files.readString(Paths.get(options.getInputFileName()));
             }
-
-            if (!dataDefined) {
-                if (fileInput) {
-                    data = new String(Files.readAllBytes(Paths.get(inputFileName)));
-                } else {
-                    throw new IllegalArgumentException();
-                }
-            }
-
-            if (mode.equals("enc")) {
-                data = encrypt(data, key);
-            } else if (mode.equals("dec")) {
-                data = decrypt(data, key);
-            }
-
-            if (fileOutput) {
-                Path path = Paths.get(outputFileName);
-                Files.write(
-                        path,
-                        data.getBytes(),
+            String resultData = context.operate(data, options.getKey());
+            if (options.isOutputFileDefined()) {
+                Files.writeString(
+                        Paths.get(options.getOutputFileName()),
+                        resultData,
                         StandardOpenOption.WRITE,
-                        StandardOpenOption.CREATE,
                         StandardOpenOption.TRUNCATE_EXISTING
                 );
             } else {
-                System.out.println(data);
+                System.out.println(resultData);
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: invalid parameters!");
-        } catch (IOException e) {
-            System.out.println("Error: can't read/write file!");
+        } catch (RuntimeException | IOException e) {
+            System.err.println(e);
         }
     }
+}
 
-    private static String decrypt(String data, int key) {
+class Options {
+    private Mode mode;
+    private CipherAlgorithm algorithm;
+    private int key;
+    private boolean isDataDefined;
+    private String data;
+    private boolean isInputFileDefined;
+    private String inputFileName;
+    private String outputFileName;
+    private boolean isOutputFileDefined;
+
+    private Options() {
+        this.mode = Mode.ENCRYPTION;
+        this.algorithm = new ShiftAlgorithm();
+        this.key = 5;
+        this.isDataDefined = false;
+        this.data = "";
+        this.isInputFileDefined = false;
+        this.inputFileName = "";
+        this.isOutputFileDefined = false;
+        this.outputFileName = "";
+    }
+
+    public static Options fromArgs(String[] args) {
+        if (args == null || args.length == 0) {
+            throw new RuntimeException("Arguments are null or empty!");
+        }
+
+        Options options = new Options();
+        for (int i = 0; i < args.length - 1; i += 2) {
+            String key = args[i];
+            String value = args[i + 1];
+            switch (key) {
+                case "-mode":
+                    if (value.equalsIgnoreCase("dec")) {
+                        options.setMode(Mode.DECRYPTION);
+                    } else if (value.equalsIgnoreCase("enc")) {
+                        options.setMode(Mode.ENCRYPTION);
+                    } else {
+                        throw new RuntimeException("Unknown mode: " + value);
+                    }
+                    break;
+                case "-alg":
+                    if (value.equalsIgnoreCase("unicode")) {
+                        options.setAlgorithm(new UnicodeAlgorithm());
+                    } else if (value.equalsIgnoreCase("shift")) {
+                        options.setAlgorithm(new ShiftAlgorithm());
+                    } else {
+                        throw new RuntimeException("Unknown algorithm: " + value);
+                    }
+                    break;
+                case "-key":
+                    options.setKey(Integer.parseInt(value));
+                    break;
+                case "-data":
+                    if (options.isInputFileDefined) {
+                        throw new RuntimeException(
+                                "Ambiguous behavior: defining data when input file was defined!"
+                        );
+                    }
+                    options.setDataDefined(true);
+                    options.setData(value);
+                    break;
+                case "-in":
+                    if (options.isDataDefined) {
+                        throw new RuntimeException(
+                                "Ambiguous behavior: defining input file when data was defined!"
+                        );
+                    }
+                    options.setInputFileDefined(true);
+                    options.setInputFileName(value);
+                    break;
+                case "-out":
+                    options.setOutputFileDefined(true);
+                    options.setOutputFileName(value);
+                    break;
+                default:
+                    throw new RuntimeException("Parsing error. Unknown key: " + key);
+            }
+        }
+        return options;
+    }
+
+    public boolean isDataDefined() {
+        return isDataDefined;
+    }
+
+    public void setDataDefined(boolean dataDefined) {
+        isDataDefined = dataDefined;
+    }
+
+    public Mode getMode() {
+        return mode;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
+
+    public CipherAlgorithm getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(CipherAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isInputFileDefined() {
+        return isInputFileDefined;
+    }
+
+    public void setInputFileDefined(boolean inputFileDefined) {
+        isInputFileDefined = inputFileDefined;
+    }
+
+    public String getInputFileName() {
+        return inputFileName;
+    }
+
+    public void setInputFileName(String inputFileName) {
+        this.inputFileName = inputFileName;
+    }
+
+    public String getOutputFileName() {
+        return outputFileName;
+    }
+
+    public void setOutputFileName(String outputFileName) {
+        this.outputFileName = outputFileName;
+    }
+
+    public boolean isOutputFileDefined() {
+        return isOutputFileDefined;
+    }
+
+    public void setOutputFileDefined(boolean outputFileDefined) {
+        isOutputFileDefined = outputFileDefined;
+    }
+}
+
+class Context {
+    private Mode mode;
+    private CipherAlgorithm algorithm;
+
+
+    public Context(Mode mode, CipherAlgorithm algorithm) {
+        this.mode = mode;
+        this.algorithm = algorithm;
+    }
+
+    public String operate(String data, int key) {
+        switch (mode) {
+            case DECRYPTION:
+                return algorithm.decrypt(data, key);
+            case ENCRYPTION:
+                return algorithm.encrypt(data, key);
+            default:
+                throw new RuntimeException("Invalid context mode: " + mode);
+        }
+    }
+}
+
+enum Mode {
+    ENCRYPTION,
+    DECRYPTION
+}
+
+interface CipherAlgorithm {
+    String encrypt(String data, int key);
+    String decrypt(String data, int key);
+}
+
+class UnicodeAlgorithm implements CipherAlgorithm {
+
+    @Override
+    public String encrypt(String data, int key) {
+        StringBuilder builder = new StringBuilder();
+        for (char c : data.toCharArray()) {
+            builder.append((char)(c + key));
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String decrypt(String data, int key) {
         StringBuilder builder = new StringBuilder();
         for (char c : data.toCharArray()) {
             builder.append((char)(c - key));
         }
         return builder.toString();
     }
+}
 
-    private static String encrypt(String data, int key) {
-        StringBuilder builder = new StringBuilder();
-        for (char c : data.toCharArray()) {
-            builder.append((char)(c + key));
+class ShiftAlgorithm implements CipherAlgorithm {
+
+    @Override
+    public String encrypt(String data, int key) {
+        StringBuilder result = new StringBuilder();
+        char[] chars = data.toCharArray();
+        for (char aChar : chars) {
+            CharType type = getCharType(aChar);
+            if (type == CharType.LOW_LETTER) {
+                aChar = (char) ((aChar - 97 + key) % 26 + 97);
+            } else if (type == CharType.UPPER_LETTER) {
+                aChar = (char) ((aChar - 65 + key) % 26 + 65);
+            }
+            result.append(aChar);
         }
-        return builder.toString();
+        return result.toString();
+    }
+
+    @Override
+    public String decrypt(String data, int key) {
+        key = 26 - (key % 26);
+        return encrypt(data, key);
+    }
+
+    private CharType getCharType(char aChar) {
+        if (aChar >= 97 && aChar <= 122) {
+            return CharType.LOW_LETTER;
+        } else if (aChar >= 65 && aChar <= 90) {
+            return CharType.UPPER_LETTER;
+        } else {
+            return CharType.NOT_LETTER;
+        }
+    }
+
+    private enum CharType {
+        LOW_LETTER,
+        UPPER_LETTER,
+        NOT_LETTER
     }
 }
